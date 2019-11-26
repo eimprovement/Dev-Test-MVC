283e77b5127667df4a48a64eee2ea8a486d19253
Carter Mitchell <carter-mitchell@outlook.com>
2019-11-25 20:30:39 -0600

    Create Domain project with initial types and mock data service

---
+++ eimprovement.WebApplication/eimprovement.Domain/Services/IPetStoreData.cs
@@ -0,0 +1,19 @@
+﻿using eimprovement.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eimprovement.Domain.Services
+{
+    public interface IPetStoreData
+    {
+        IEnumerable<Pet> GetAll();
+        Pet Get(Int64 id);
+        IEnumerable<Pet> FindByStatus(string status);
+        Int64 Add(Pet pet);
+        void UpdatePet(Int64 petId);
+        void Delete(Int64 id);
+    }
+}
---
+++ eimprovement.WebApplication/eimprovement.Domain/Services/MockDataSource.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eimprovement.Domain.Models;
+
+namespace eimprovement.Domain.Services
+{
+    public class MockDataSource : IPetStoreData
+    {
+        public MockDataSource()
+        {
+
+        }
+
+        public long Add(Pet pet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Pet> FindByStatus(string status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pet Get(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Pet> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePet(long petId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
