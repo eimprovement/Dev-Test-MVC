387a77f4d7c5e5e4e96b19ebafe5062e994e6760
Carter Mitchell <carter-mitchell@outlook.com>
2019-11-25 21:06:08 -0600

    Tweaked layouit view and home page view for brand. Installed Autofac for dependency injection for mock database source.

---
+++ eimprovement.WebApplication/eimprovement.WebApplication/App_Start/ContainerConfig.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using eimprovement.Domain.Services;
+using System.Web.Mvc;
+
+namespace eimprovement.WebApplication
+{
+    public class ContainerConfig
+    {
+        internal static void RegisterContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // for development since all data inside one object 
+            // specify singleton to maintain data consistency
+            // Change Instance type later when swap in new data source
+            builder.RegisterType<MockDataSource>()
+                .As<IPetStoreData>()
+                .SingleInstance();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
--- eimprovement.WebApplication/eimprovement.WebApplication/Global.asax.cs
+++ eimprovement.WebApplication/eimprovement.WebApplication/Global.asax.cs
@@ -18,6 +18,8 @@ namespace eimprovement.WebApplication
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ContainerConfig.RegisterContainer();
         }
     }
+
 }
