b0b32bb599d30d03f2d5cf6592241ca1c2fbd16e
Carter Mitchell <carter-mitchell@outlook.com>
2019-11-25 20:50:35 -0600

    Add PetsController, related models, helper classes

---
+++ eimprovement.WebApplication/eimprovement.WebApplication/Helpers/StringEnum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+
+namespace eimprovement.WebApplication.Helpers
+{
+    public class StringEnum
+    {
+        public static string GetStringFromEnum(Enum value)
+        {
+            string output = null;
+            Type type = value.GetType();
+
+            FieldInfo fi = type.GetField(value.ToString());
+            StringValue[] attrs = fi.GetCustomAttributes(typeof(StringValue), false) as StringValue[];
+
+            if (attrs.Length > 0)
+            {
+                output = attrs[0].Value;
+            }
+
+            return output;
+        }
+    }
+}
---
+++ eimprovement.WebApplication/eimprovement.WebApplication/Helpers/StringValue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace eimprovement.WebApplication.Helpers
+{
+    public class StringValue : Attribute
+    {
+        private readonly string _value;
+
+        public StringValue(string value)
+        {
+            _value = value;
+        }
+
+        public string Value
+        {
+            get { return _value; }
+        }
+    }
+}
